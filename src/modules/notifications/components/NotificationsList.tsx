818a0c6eecda1fe75d26701593a7a7dfc03ac399
Samuel Salami <salamisamuel88@yahoo.com>
2025-08-28 06:41:55 +0100

    Fix missing key prop in NotificationsList

--- src/modules/notifications/components/NotificationsList.tsx
+++ src/modules/notifications/components/NotificationsList.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import NotificationCard from "./NotificationCard";
 import { notificationsData } from "../utils/constants";
 
@@ -5,17 +6,16 @@ const NotificationsList = () => {
   return (
     <section className="w-full bg-white rounded-xl">
       {notificationsData.map((notification, index) => (
-        <>
+        <React.Fragment key={notification.id}>
           <div className="py-5 px-4.5">
             <NotificationCard
-              key={notification.id}
               notification={notification}
             />
           </div>
           {notificationsData.length - 1 !== index && (
             <hr className="h-0.5 border-b border-b-[#FFF8F8]" />
           )}
-        </>
+        </React.Fragment>
       ))}
     </section>
   );
